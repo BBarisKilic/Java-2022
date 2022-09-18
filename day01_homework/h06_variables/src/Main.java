579b85008b480f09b47e2b91c5058b3282edb3ee
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-18 14:05:25 +0300

    Added h06_variables.

---
+++ day01_homework/h06_variables/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // String variable
+        String metin = "Ögrenci sayim: ";
+
+        // Integer variable
+        int ogrenciSayisi = 10;
+
+        System.out.println(metin + ogrenciSayisi);
+        System.out.println(metin + ogrenciSayisi);
+        System.out.println(metin + ogrenciSayisi);
+        System.out.println(metin + ogrenciSayisi);
+    }
+}
