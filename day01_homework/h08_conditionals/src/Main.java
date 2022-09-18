b5d45c282eb0a5d15e29bb5f6a13e2a2e7b38272
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-18 14:34:40 +0300

    Added h08_conditionals.

---
+++ day01_homework/h08_conditionals/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 20;
+
+        if(sayi<20) {
+            System.out.println("Sayi 20'den kücüktür!");
+        } else if (sayi>20) {
+            System.out.println("Sayi 20'den büyüktür!");
+        } else {
+            System.out.println("Sayi 20'ye esittir!");
+        }
+    }
+}
