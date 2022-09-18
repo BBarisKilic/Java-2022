27c05f4f35a79c58871e36692ba1a60d8832bc68
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-18 14:16:32 +0300

    Added h07_data_types.

---
+++ day01_homework/h07_data_types/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Byte type -> 1 Byte
+        byte byteSayi = 127;
+        System.out.println(byteSayi);
+
+        // Short type -> 2 Bytes
+        short shortSayi = 32767;
+        System.out.println(shortSayi);
+
+        // Integer type -> 4 bytes
+        int sayi = 20;
+        System.out.println(sayi);
+
+        // Double type -> 8 bytes
+        double doubleSayi = 2.3;
+        System.out.println(doubleSayi);
+
+        // Char type -> 2 bytes
+        char karakter = 'A';
+        System.out.println(karakter);
+
+        // Boolean type -> 1 Byte
+        boolean dogruMu = true;
+        System.out.println(dogruMu);
+    }
+}
