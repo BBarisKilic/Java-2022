9344a0d5a4e562dff286e3cb2928130e68282dbb
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-20 12:44:39 +0300

    Added h11_for_loop.

---
+++ day01_homework/h11_for_loop/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Döngü tamamlandi.");
+    }
+}
