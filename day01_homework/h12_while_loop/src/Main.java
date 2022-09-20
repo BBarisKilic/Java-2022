c313c3dee40c0d78510d556cc5e3d41926f9bc33
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-20 12:49:22 +0300

    Added h12_while_loop.

---
+++ day01_homework/h12_while_loop/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while(i<10) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("Döngü tamamlandi!");
+    }
+}
