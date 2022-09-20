fe3f7727dc06d62493d2040bb75c6a53add5d060
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-20 12:55:31 +0300

    Added h13_do_while_loop.

---
+++ day01_homework/h13_do_while_loop/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 100;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while(i<10);
+
+        System.out.println("Döngü tamamlandi!");
+    }
+}
