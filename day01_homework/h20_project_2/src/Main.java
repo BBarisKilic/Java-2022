535fa4e6dec7bfcd759de05ec40fde33be38b6b8
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-21 14:36:23 +0300

    Added h20_project_2.

---
+++ day01_homework/h20_project_2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char harf = 'E';
+
+        switch (harf) {
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalin sesli harf");
+                break;
+            default:
+                System.out.println("Ince sesli harf");
+        }
+    }
+}
