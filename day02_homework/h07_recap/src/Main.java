658110dce7631f4e7d4ba446eed8b89e86e74e07
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-25 22:44:25 +0300

    Added h07_recap.

---
+++ day02_homework/h07_recap/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = dortIslem.Topla(4,5);
+        System.out.println(sonuc);
+    }
+}
