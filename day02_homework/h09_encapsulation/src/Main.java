b5f142c8a21c2770a99e3f7f131b76e08f484336
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-09-28 14:25:31 +0300

    Added h09_encapsulation.

---
+++ day02_homework/h09_encapsulation/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product();
+        ProductManager productManager = new ProductManager();
+
+        productManager.add(product);
+    }
+}
---
+++ day02_homework/h09_encapsulation/src/Product.java
@@ -0,0 +1,7 @@
+public class Product {
+    private int id;
+    private String name;
+    private String decription;
+    private double price;
+    private int stockAmount;
+}
